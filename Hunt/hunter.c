ce746b74ce6c46156721169dc02e302569b55b94
JAMESPC\James <jdowers20@gmail.com>
2016-10-10 10:48:15 +1100

    Did some more work on the hunter, it now moves randomly around the map

--- Hunt/hunter.c
+++ Hunt/hunter.c
@@ -12,27 +12,31 @@ void decideHunterMove(HunterView gameState)
     // Replace the line below by something better
 	char * location = malloc(2*sizeof(char));
 	char * message = malloc(MESSAGE_SIZE*sizeof(char));
-	playerID player = whoAmI(gameState);
+	PlayerID player = whoAmI(gameState);
 	int maxRail = ((int)player + giveMeTheRound(gameState)) % 4;
 
 	//If I can see any of draculas recent locations, pop "H**" with ** being the location
-	char * trail = malloc(8*TRAIL_SIZE*sizeof(char));
+	LocationID * trail = malloc(TRAIL_SIZE*sizeof(LocationID));
 	giveMeTheTrail(gameState, player, trail);
 
 	int i;
 	for (i = 0; trail[i] != '\0'; i++){
-		if (i%8 == 0){
-			if (trail[i] == 'D' && trail[i+2] != '?'){
-				message[0] = 'H';
-				message[1] = trail[i+1];
-				message[2] = trail[i+2];
-				message[3] = '\0';
-			}
+		if (trail[i] != -1){
+			message[0] = 'H';
+			message[1] = (char)(trail[i]/10 +'0');
+			message[2] = (char)(trail[i]%10 +'0');
+			message[3] = '\0';
 		}
 	}
 
 	//Move to a random spot
-	
+	int numLocs;
+	LocationID *locations = whereCanIgo(gameState, &numLocs, TRUE, maxRail, TRUE);
+
+	srand(69);
+
+	LocationID selection = locations[rand() % numLocs];
+	location = idToName(selection);
 
     registerBestPlay(location, message);
 }
