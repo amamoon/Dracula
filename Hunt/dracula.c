ecbbf186963bcd592f6973ddae982de3f303916e
amamoon <akeefmamoon@gmail.com>
2016-10-13 01:17:53 +1100

    dracula.c
    added some changes so it actually does something now
    most of its psuedo code

--- Hunt/dracula.c
+++ Hunt/dracula.c
@@ -8,7 +8,80 @@
 
 void decideDraculaMove(DracView gameState)
 {
-	// TODO ...
-	// Replace the line below by something better
-	registerBestPlay("CD","Mwuhahahaha");
+	
+int inTrail(LocationID location, DracView gameState);
+
+void decideDraculaMove(DracView gameState)
+{
+    int roundNumber = giveMeTheRound(gameState);
+    
+    // For random number generation
+    srand(time(NULL));
+    
+    if (roundNumber == 0) {
+        
+#ifdef DEBUG
+        printf("a\n");
+#endif
+        
+        // Go to predermined location
+        registerBestPlay("CD","");
+        
+    } else {
+        
+#ifdef DEBUG
+        printf("b\n");
+#endif
+        LocationID *possibleLocations;
+        int numPossibleLocations;
+        
+        possibleLocations = whereCanIgo(gameState, &numPossibleLocations, TRUE, TRUE);
+        
+        LocationID locationToGo = possibleLocations[0];
+        
+        int index = 0;
+        
+        int foundLocation = FALSE;
+        
+        while (index < numPossibleLocations && !foundLocation) {
+            printf("In here\n");
+            if (!inTrail(possibleLocations[index], gameState)) {
+                locationToGo = possibleLocations[index];
+                foundLocation = TRUE;
+            }
+            index++;
+        }
+        
+        
+        /*int randomPosition = rand()%numPossibleLocations;
+        
+        if (randomPosition == 0) {
+            randomPosition++;
+        } else if (randomPosition >= numPossibleLocations) {
+            randomPosition--;
+        }
+        
+        LocationID locationToGo = possibleLocations[randomPosition];*/
+        
+        registerBestPlay(idToAbbrev(locationToGo) ,"");
+        
+    }
+
+}
+
+
+int inTrail(LocationID location, DracView gameState) {
+    LocationID trail[TRAIL_SIZE] = {0};
+    giveMeTheTrail(gameState, PLAYER_DRACULA, trail);
+    
+    int i;
+    
+    for (i = 0; i < TRAIL_SIZE; i++) {
+        if (trail[i] == location) {
+            return TRUE;
+        }
+    }
+    
+    return FALSE;
+}
 }
